868d6982cada5872d204914287b0074f2a970852
dimarval <dimarva.90@gmail.com>
2014-01-06 17:19:49 -0600

    Refactor ReservationSystem class.

--- lib/motel/reservations/reservation_system.rb
+++ lib/motel/reservations/reservation_system.rb
@@ -5,55 +5,12 @@ module Motel
 
     class ReservationSystem
 
-      attr_accessor :source
+      def self.source(source_type, config)
+        source_class = "Motel::Reservations::Sources::#{source_type.to_s.camelize}".constantize
 
-      def initialize
-        @source = Sources::Default.new
-      end
+        source_instance = source_class.new(config)
 
-      def source_configurations(source)
-        source_class = "Motel::Reservations::Sources::#{source.to_s.camelize}".constantize
-
-        source_instance = source_class.new
-
-        yield source_instance if block_given?
-
-        self.source = source_instance
-      end
-
-      def tenants
-        source.tenants
-      end
-
-      def tenant(name)
-        source.tenant(name)
-      end
-
-      def tenant?(name)
-        source.tenant?(name)
-      end
-
-      def add_tenant(name, spec, expiration = nil)
-        source.add_tenant(name, spec, expiration)
-        tenant?(name)
-      end
-
-      def update_tenant(name, spec, expiration = nil)
-        source.update_tenant(name, spec, expiration)
-        tenant(name)
-      end
-
-      def delete_tenant(name)
-        source.delete_tenant(name)
-        !tenant?(name)
-      end
-
-      def create_tenant_table
-        source.create_tenant_table
-      end
-
-      def destroy_tenant_table
-        source.destroy_tenant_table
+        source_instance
       end
 
     end
