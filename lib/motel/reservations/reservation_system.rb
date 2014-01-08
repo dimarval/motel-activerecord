6b2ede18f4835dbf22f9d52f466618bbf9867b40
dimarval <dimarva.90@gmail.com>
2014-01-08 16:21:10 -0600

    Refactor reservation systema class.

--- lib/motel/reservations/reservation_system.rb
+++ lib/motel/reservations/reservation_system.rb
@@ -5,12 +5,20 @@ module Motel
 
     class ReservationSystem
 
-      def self.source(source_type, config = {})
+      def self.source=(source)
+        @@source = source
+      end
+
+      def self.source
+        @@source ||= Sources::Default.new
+      end
+
+      def self.source_configurations(source_type, config = {})
         source_class = "Motel::Reservations::Sources::#{source_type.to_s.camelize}".constantize
 
         source_instance = source_class.new(config)
 
-        source_instance
+        self.source = source_instance
       end
 
     end
--- spec/lib/motel/reservations/reservation_system_spec.rb
+++ spec/lib/motel/reservations/reservation_system_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 describe Motel::Reservations::ReservationSystem do
 
+  before(:all) do
+    @reservation_system = Motel::Reservations::ReservationSystem
+  end
+
+  after(:all) do
+    Motel::Reservations::ReservationSystem.source = nil #sets default source
+  end
+
   describe '#source_configurations' do
 
     context 'redis source' do
 
       before(:all) do
-        @source = Motel::Reservations::ReservationSystem.source(:redis, {
+        @reservation_system.source_configurations(:redis, {
           host:                'localhost',
           port:                6380,
           password:            'none',
@@ -17,15 +25,15 @@ describe Motel::Reservations::ReservationSystem do
       end
 
       it 'places a redis instance on the source' do
-        expect(@source).to be_an_instance_of Motel::Reservations::Sources::Redis
+        expect(@reservation_system.source).to be_an_instance_of Motel::Reservations::Sources::Redis
       end
 
       it 'source attributes has a correct values' do
-        expect(@source.host).to                eq 'localhost'
-        expect(@source.port).to                eq 6380
-        expect(@source.password).to            eq 'none'
-        expect(@source.path).to                eq '/tmp/redis.sock'
-        expect(@source.prefix_tenant_alias).to eq 'test-tenant'
+        expect(@reservation_system.source.host).to                eq 'localhost'
+        expect(@reservation_system.source.port).to                eq 6380
+        expect(@reservation_system.source.password).to            eq 'none'
+        expect(@reservation_system.source.path).to                eq '/tmp/redis.sock'
+        expect(@reservation_system.source.prefix_tenant_alias).to eq 'test-tenant'
       end
 
     end
@@ -33,19 +41,19 @@ describe Motel::Reservations::ReservationSystem do
     context 'database source' do
 
       before(:all) do
-        @source = Motel::Reservations::ReservationSystem.source(:database, {
+        @reservation_system.source_configurations(:database, {
           source_spec: TENANTS_SPEC,
           table_name:  'tenant'
         })
       end
 
       it 'places a database instance on the source' do
-        expect(@source).to be_an_instance_of Motel::Reservations::Sources::Database
+        expect(@reservation_system.source).to be_an_instance_of Motel::Reservations::Sources::Database
       end
 
       it 'source attributes has a correct values' do
-        expect(@source.source_spec).to eq TENANTS_SPEC
-        expect(@source.table_name).to  eq 'tenant'
+        expect(@reservation_system.source.source_spec).to eq TENANTS_SPEC
+        expect(@reservation_system.source.table_name).to  eq 'tenant'
       end
 
     end
