81e7c3e2361d436d4fb68e903436de0f0e797a8b
dimarval <dimarva.90@gmail.com>
2014-01-11 18:08:32 -0600

    Override initializer of ActiveRecord to set reloader hooks.

--- lib/motel/railtie.rb
+++ lib/motel/railtie.rb
@@ -5,6 +5,9 @@ require 'rails'
 module Motel
 
   class Railtie < Rails::Railtie
+    INIT_TO_DELETE = %w(active_record.initialize_database active_record.set_reloader_hooks)
+
+
     config.motel = ActiveSupport::OrderedOptions.new
     config.disable_motel_middleware = false
 
@@ -12,23 +15,31 @@ module Motel
       namespace :db do
         task :load_config do
           if defined? ActiveRecord::Tasks::DatabaseTasks
-            ActiveRecord::Tasks::DatabaseTasks.env = ENV['TENANT'] || ActiveRecord::Base.motel.default_tenant
+            ActiveRecord::Tasks::DatabaseTasks.env = ActiveRecord::Base.motel.determines_tenant
           end
         end
       end
     end
 
-    initializer "motel.general_configuration" do
-      ActiveRecord::Base.motel.nonexistent_tenant_page ||= begin
-        config.motel.nonexistent_tenant_page || 'public/404.html'
-      end
-      ActiveRecord::Base.motel.default_tenant = Rails.application.config.motel.default_tenant
-      ActiveRecord::Base.motel.current_tenant = Rails.application.config.motel.current_tenant
-      ActiveRecord::Base.motel.admission_criteria = Rails.application.config.motel.admission_criteria
+    ActiveRecord::Railtie.initializers.delete_if do |i|
+      INIT_TO_DELETE.include?(i.name)
+    end
 
-      source_configurations = Rails.application.config.motel.tenants_source_configurations
-      if source_configurations
-        ActiveRecord::Base.motel.tenants_source_configurations(source_configurations)
+    initializer "motel.general_configuration" do
+      motel_config = Rails.application.config.motel
+
+      ActiveRecord::Base.motel.nonexistent_tenant_page ||= begin
+        motel_config.nonexistent_tenant_page || 'public/404.html'
+      end
+
+      ActiveRecord::Base.motel.default_tenant = motel_config.default_tenant
+      ActiveRecord::Base.motel.current_tenant = motel_config.current_tenant
+      ActiveRecord::Base.motel.admission_criteria = motel_config.admission_criteria
+
+      if motel_config.tenants_source_configurations
+        ActiveRecord::Base.motel.tenants_source_configurations(
+          tenants_source_configurations
+        )
       end
     end
 
@@ -39,8 +50,17 @@ module Motel
       end
     end
 
-    ActiveRecord::Railtie.initializers.delete_if do |i|
-      i.name == "active_record.initialize_database"
+    initializer "active_record.set_reloader_hooks" do |app|
+      hook = app.config.reload_classes_only_on_change ? :to_prepare : :to_cleanup
+
+      ActiveSupport.on_load(:active_record) do
+        ActionDispatch::Reloader.send(hook) do
+          if ActiveRecord::Base.motel.active_tenants.any?
+            ActiveRecord::Base.clear_reloadable_connections!
+            ActiveRecord::Base.clear_cache!
+          end
+        end
+      end
     end
 
   end
