d1fd549fbc2a98f80e9e59191e501201d48e0442
dimarval <dimarva.90@gmail.com>
2014-01-17 12:13:53 -0600

    Update Railtie to initialize and config Rails tasks.

--- lib/motel/railtie.rb
+++ lib/motel/railtie.rb
@@ -7,16 +7,19 @@ module Motel
   class Railtie < Rails::Railtie
     INIT_TO_DELETE = %w(active_record.initialize_database active_record.set_reloader_hooks)
 
-
     config.motel = ActiveSupport::OrderedOptions.new
-    config.disable_motel_middleware = false
 
     rake_tasks do
       namespace :db do
         task :load_config do
-          if defined? ActiveRecord::Tasks::DatabaseTasks
-            ActiveRecord::Tasks::DatabaseTasks.env = ActiveRecord::Base.motel.determines_tenant
-          end
+          Motel::Manager.tenants_source_configurations(
+            Rails.application.config.motel.tenants_source_configurations
+          )
+
+          Motel::Manager.current_tenant = "ActiveRecord::Base"
+
+          ActiveRecord::Tasks::DatabaseTasks.database_configuration = Motel::Manager.tenants
+          ActiveRecord::Tasks::DatabaseTasks.env = Motel::Manager.determines_tenant
         end
       end
     end
@@ -28,24 +31,15 @@ module Motel
     initializer "motel.general_configuration" do
       motel_config = Rails.application.config.motel
 
-      ActiveRecord::Base.motel.nonexistent_tenant_page ||= begin
-        motel_config.nonexistent_tenant_page || 'public/404.html'
-      end
-
-      ActiveRecord::Base.motel.default_tenant = motel_config.default_tenant
-      ActiveRecord::Base.motel.current_tenant = motel_config.current_tenant
-      ActiveRecord::Base.motel.admission_criteria = motel_config.admission_criteria
-
-      if motel_config.tenants_source_configurations
-        ActiveRecord::Base.motel.tenants_source_configurations(
-          motel_config.tenants_source_configurations
-        )
-      end
+      Motel::Manager.nonexistent_tenant_page = motel_config.nonexistent_tenant_page || 'public/404.html'
+      Motel::Manager.admission_criteria = motel_config.admission_criteria
+      Motel::Manager.default_tenant = motel_config.default_tenant
+      Motel::Manager.current_tenant = motel_config.current_tenant
+      Motel::Manager.tenants_source_configurations(motel_config.tenants_source_configurations)
     end
 
     initializer "motel.configure_middleware" do |app|
-      disable_middleware = Rails.application.config.disable_motel_middleware || false
-      unless disable_middleware
+      unless Rails.application.config.motel.disable_middleware
         app.config.middleware.insert_before ActiveRecord::Migration::CheckPending, Lobby
       end
     end
