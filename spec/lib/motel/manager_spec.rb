edf1947e40dd68da0c3e119804040b0b7a9ae8fc
dimarval <dimarva.90@gmail.com>
2014-01-17 10:27:31 -0600

    Add examples to manager_sepc file.

--- spec/lib/motel/manager_spec.rb
+++ spec/lib/motel/manager_spec.rb
@@ -33,32 +33,60 @@ describe Motel::Manager do
 
     context 'redis source' do
 
-      it 'tenants_source in connecion_handler of active record is an instance of redis' do
+      before(:each) do
         @manager.tenants_source_configurations({
-          source: :redis,
-          hots:   'localhost',
-          port:   6379
+          source:              :redis,
+          host:                'localhost',
+          port:                6380,
+          password:            'none',
+          path:                '/tmp/redis.sock',
+          prefix_tenant_alias: 'test-tenant'
         })
+      end
 
-        expect(
-          @manager.tenants_source
-        ).to be_an_instance_of Motel::Sources::Redis
+      it 'places a redis instance on the source' do
+        expect(@manager.tenants_source).to be_an_instance_of Motel::Sources::Redis
+      end
+
+      it 'source attributes has a correct values' do
+        expect(@manager.tenants_source.host).to                eq 'localhost'
+        expect(@manager.tenants_source.port).to                eq 6380
+        expect(@manager.tenants_source.password).to            eq 'none'
+        expect(@manager.tenants_source.path).to                eq '/tmp/redis.sock'
+        expect(@manager.tenants_source.prefix_tenant_alias).to eq 'test-tenant'
       end
 
     end
 
     context 'database source' do
 
-      it 'tenants_source in connecion_handler of active record is an instance of database' do
+      before(:each) do
         @manager.tenants_source_configurations({
           source:      :database,
-          spec:        TENANTS_SPEC,
-          table_name: 'tenant'
+          source_spec: TENANTS_SPEC,
+          table_name:  'tenant'
         })
+      end
 
-        expect(
-          @manager.tenants_source
-        ).to be_an_instance_of Motel::Sources::Database
+      it 'places a database instance on the source' do
+        expect(@manager.tenants_source).to be_an_instance_of Motel::Sources::Database
+      end
+
+      it 'source attributes has a correct values' do
+        expect(@manager.tenants_source.source_spec).to eq TENANTS_SPEC
+        expect(@manager.tenants_source.table_name).to  eq 'tenant'
+      end
+
+    end
+
+    context 'default source' do
+
+      before(:each) do
+        @manager.tenants_source_configurations({source: :default})
+      end
+
+      it 'places a default instance on the source' do
+        expect(@manager.tenants_source).to be_an_instance_of Motel::Sources::Default
       end
 
     end
